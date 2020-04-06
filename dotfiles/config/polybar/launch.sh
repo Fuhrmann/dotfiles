38c6fa6b50af56d2d3108c7da192b70e52621c65
Fuhrmann <fuhrmanns@gmail.com>
2020-04-06 00:27:32 -0400

    Updating

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -4,7 +4,15 @@
 killall -q polybar
 
 # Wait for the processes shut down
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch
-polybar main -c ~/.config/polybar/config.ini &
+if type "xrandr"; then
+  export TRAYPOS = right
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c ~/.config/polybar/config.ini &
+    unset TRAYPOS
+  done
+else
+  polybar --reload main -c ~/.config/polybar/config.ini & 
+fi
