af78f8fb2fef1012ff6b7a31fe825bb951df50ac
Fuhrmann <fuhrmanns@gmail.com>
2020-04-05 21:27:44 -0400

    Add playerctl script

---
+++ dotfiles/config/polybar/scripts/playerctl_playing.sh
@@ -0,0 +1,10 @@
+
+#!/bin/sh
+
+player_status=$(playerctl status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo "%{T3}%{T-} $(playerctl metadata artist) - $(playerctl metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo "%{T3}%{T-} $(playerctl metadata artist) - $(playerctl metadata title)"
+fi
