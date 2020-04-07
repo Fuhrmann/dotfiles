4d40631300c3269c94880ec85c65f12f35c9f993
Fuhrmann <fuhrmanns@gmail.com>
2020-04-07 12:50:24 -0400

    Adding new files and updating anothers

--- dotfiles/config/polybar/scripts/playerctl_playing.sh
+++ dotfiles/config/polybar/scripts/playerctl_playing.sh
@@ -1,10 +1,15 @@
 
 #!/bin/sh
 
-player_status=$(playerctl -i chrome status 2> /dev/null)
+player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
     echo " $(playerctl -i chrome metadata artist) - $(playerctl -i chrome metadata title)"
 elif [ "$player_status" = "Paused" ]; then
     echo " $(playerctl -i chrome metadata artist) - $(playerctl -i chrome metadata title)"
+elif [ "$player_status" = "Stopped" ]; then
+		mpd_current=$(mpc current 2> /dev/null)
+		if [[ $(echo "$mpd_current" | wc -l) -gt 0 ]]; then
+		    echo " $mpd_current"
+    fi
 fi
--- dotfiles/config/polybar/scripts/updates-fedora.sh
+++ dotfiles/config/polybar/scripts/updates-fedora.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-dnf=$(env LC_ALL=C sudo dnf upgrade --refresh --assumeno 2> /dev/null)
+BAR_ICON=""
 
-upgrade=$(echo "$dnf" | grep '^Upgrade ' | awk '{ print $2 }')
-install=$(echo "$dnf" | grep '^Install ' | awk '{ print $2 }')
+get_total_updates() {
+	dnf=$(env LC_ALL=C dnf check-update 2> /dev/null)
+	UPDATES=$(echo "$dnf" | grep -E '[^@]updates' | wc -l)
+}
 
-updates=$(( upgrade + install ))
 
-if [ "$updates" -gt 0 ]; then
-    echo " $updates"
-else
-    echo ""
-fi
+while true; do
+    get_total_updates
+
+    # notify user of updates
+    if (( UPDATES > 50 )); then
+        notify-send -u critical "You really need to update!!" "$UPDATES New packages"
+    elif (( UPDATES > 25 )); then
+        notify-send -u normal "You should update soon" "$UPDATES New packages"
+    elif (( UPDATES > 2 )); then
+        notify-send -u low "$UPDATES New packages to update"
+    fi
+
+    # when there are updates available
+    # every 10 seconds another check for updates is done
+    if (( UPDATES > 0 )); then
+        echo "$BAR_ICON $UPDATES"
+    else
+        echo $BAR_ICON
+    fi
+
+    sleep 1800
+done
