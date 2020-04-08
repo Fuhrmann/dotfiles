56e5700c2053c297cd7a82c6a35ffa83f4b64db1
Fuhrmann <fuhrmanns@gmail.com>
2020-04-08 03:15:31 -0400

    Updating

--- dotfiles/config/i3/scripts/open_music.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-count=`ps aux | grep -c ncspot`
-if [ $count -eq 1 ]; then
-    kitty --title "music" --session $HOME/projetos/dotfiles/dotfiles/config/kitty/ncspot-session.kitty
-else
-    i3-msg "[title=music] focus"
-fi
